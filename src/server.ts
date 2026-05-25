1511b448478b086a8384585448b1159bccf5bbf0
Ray <firdavsyangiev@gmail.com>
2026-05-26 07:16:08 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,2 @@
-// console.log("EXECUTED");
-
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY-MM-DD");
-// console.log(currentTime);
-
-// const person: string = "Martin";
-// const count: number = 100;
-
-// Architechtural pattern: MVC, DI(Dependecy Injection), MVP
-// ^ Backend suyagi
-// MVC => Model  View  Controller
-
-// Design pattern: Middleware, Decorator
+import dotenv from "dotenv";
+dotenv.config();
