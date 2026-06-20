ea936e7fa1f7411ffef6e6971529ebb3b5867f9d
Ray <firdavsyangiev@gmail.com>
2026-06-20 18:48:05 +0900

    fix: signup process frontEnd

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,6 @@
 console.log("Signup frontend javascript file");
 
-(function () {
+$(function () {
   const fileTarget = $(".file-box .upload-hidden");
   let filename;
 
@@ -9,25 +9,21 @@ console.log("Signup frontend javascript file");
       const uploadFile = $(this)[0].files[0];
       const fileType = uploadFile["type"];
       const validImageType = ["image/jpg", "image/jpeg", "image/png"];
-
       if (!validImageType.includes(fileType)) {
-        alert("Please insert only jpeg, jpg and png!");
+        alert("Please inseart only jpeg, jpg and png!");
       } else {
         if (uploadFile) {
           console.log(URL.createObjectURL(uploadFile));
-
           $(".upload-img-frame")
             .attr("src", URL.createObjectURL(uploadFile))
             .addClass("success");
         }
-
         filename = $(this)[0].files[0].name;
       }
-
       $(this).siblings(".upload-name").val(filename);
     }
   });
-})();
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -41,20 +37,21 @@ function validateSignupForm() {
     memberPassword === "" ||
     confirmPassword === ""
   ) {
-    alert(" Please insert all required inputs!");
+    alert("Please insert all required inputs!");
     return false;
   }
 
   if (memberPassword !== confirmPassword) {
-    alert(" Password differs, please check!");
+    alert("Password differs, please check!");
     return false;
   }
 
-  const memberImage = $(".meber-image").get(0).file[0].files[0].name
+  const memberImage = $(".member-image").get(0).files[0].name
     ? $(".member-image").get(0).files[0].name
     : null;
   if (!memberImage) {
-    alert("Please insert restaurant image");
+    alert("Please insert restaurant image!");
     return false;
   }
+  return true;
 }
