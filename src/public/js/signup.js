b281e76f51797706f5d96bc0fb2fed2896ad489d
Ray <firdavsyangiev@gmail.com>
2026-06-20 18:11:40 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,30 @@
 console.log("Signup frontend javascript file");
+
+(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+
+        filename = $(this)[0].files[0].name;
+      }
+
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+})();
