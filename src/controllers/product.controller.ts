53d2c87d2a27287d518f33a77a879fe06bd83605
Ray <firdavsyangiev@gmail.com>
2026-06-23 10:46:56 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -43,14 +43,14 @@ productController.createNewProduct = async (
     await productService.createNewProduct(data);
 
     res.send(
-      `<script> alert("Succesfull creation!"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("Succesfull creation!"); window.location.replace('/admin/product/all') </script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}") window.location.replace('admin/product/all') </script>`,
+      `<script> alert("${message}") window.location.replace('/admin/product/all') </script>`,
     );
   }
 };
