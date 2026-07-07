b737f6452175d6c743c60f1ba339726c6f2b71be
Ray <firdavsyangiev@gmail.com>
2026-07-07 10:57:24 +0900

    feat: create getRestaurant rest Api

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 
 /** Member **/
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post(
@@ -22,6 +23,7 @@ router.post(
   uploader("members").single("memberImage"),
   memberController.updateMember,
 );
+router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product **/
 
